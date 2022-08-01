eb7b162d82faba58f08cffffa907f367a7b3fff7
Szymon Przebierowski <szymonprz@gmail.com>
2022-08-01 22:30:38 +0200

    unique paths

---
+++ src/main/kotlin/uniquepaths/UniquPaths.kt
@@ -0,0 +1,19 @@
+package uniquepaths
+
+class UniquePaths {
+    fun uniquePaths(m: Int, n: Int): Int {
+        val dp =  Array(m) {Array(n) {0} }
+        for(i in 0 until m){
+            dp[i][0] = 1
+        }
+        for(i in 0 until n){
+            dp[0][i] = 1
+        }
+        for(i in 1 until m){
+            for(j in 1 until n){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            }
+        }
+        return dp[m-1][n-1]
+    }
+}
